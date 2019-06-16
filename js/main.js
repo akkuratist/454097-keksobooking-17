3b44979e87c043f43135db45934680f6ffc5b3a9
akkuratist <36897138+akkuratist@users.noreply.github.com>
2019-06-16 21:48:12 +0300

    Исправления по PR

--- js/main.js
+++ js/main.js
@@ -1,16 +1,26 @@
 'use strict';
 var MIN_LOCATION_Y = 130;
 var MAX_LOCATION_Y = 630;
+var MAX_LOCATION_X = 1200;
 var MAP_PIN_HEIGHT = 70;
 var MAP_PIN_WIDTH = 50;
 var DEFAULT_OFFER_MESSAGE = 'Заголовок объявления';
 var OFFERS_TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var offersCount = 8;
 
 var mainMap = document.querySelector('.map');
 mainMap.classList.remove('map--faded');
 
 var similarOffersList = document.querySelector('.map__pins');
 var similarOfferPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+
+var getRandomNumber = function (number) {
+  return Math.random() * number;
+};
+
+var getRandomElement = function (array) {
+  return array[Math.round(getRandomNumber(array.length - 1))];
+};
 
 var createOffers = function (usersCount) {
   var similarOffers = [];
@@ -20,11 +30,11 @@ var createOffers = function (usersCount) {
       avatar: 'img/avatars/user0' + i + '.png'
     };
     similarOffer.offer = {
-      type: OFFERS_TYPE[Math.round(Math.random() * (OFFERS_TYPE.length - 1))]
+      type: getRandomElement(OFFERS_TYPE)
     };
     similarOffer.location = {
-      x: (Math.random() * 1200) - (MAP_PIN_WIDTH / 2),
-      y: MIN_LOCATION_Y + (Math.random() * (MAX_LOCATION_Y - MIN_LOCATION_Y)) - MAP_PIN_HEIGHT
+      x: (getRandomNumber(MAX_LOCATION_X)) - (MAP_PIN_WIDTH / 2),
+      y: MIN_LOCATION_Y + (getRandomNumber(MAX_LOCATION_Y - MIN_LOCATION_Y)) - MAP_PIN_HEIGHT
     };
     similarOffers.push(similarOffer);
   }
@@ -33,14 +43,15 @@ var createOffers = function (usersCount) {
 
 var renderOffer = function (similarOffer) {
   var offerElement = similarOfferPinTemplate.cloneNode(true);
+  var offerElementImage = offerElement.querySelector('img');
   offerElement.style = 'left: ' + similarOffer.location.x + 'px; ' + 'top: ' + similarOffer.location.y + 'px';
-  offerElement.querySelector('img').src = similarOffer.author.avatar;
-  offerElement.querySelector('img').alt = DEFAULT_OFFER_MESSAGE;
+  offerElementImage.src = similarOffer.author.avatar;
+  offerElementImage.alt = DEFAULT_OFFER_MESSAGE;
   return offerElement;
 };
 
 var renderOffers = function () {
-  var similarOffers = createOffers(8);
+  var similarOffers = createOffers(offersCount);
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < similarOffers.length; i++) {
     fragment.appendChild(renderOffer(similarOffers[i]));
